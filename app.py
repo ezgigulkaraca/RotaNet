c9427399db4ae9278fa7b26963fc948fd5ee2ab3
ezgigulkaraca <ezgigulkaraca@gmail.com>
2026-05-17 15:41:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
+import streamlit as st
 import google.generativeai as genai
 
-genai.configure(api_key="AIzaSyAXA7edsGE5ggJrQo5wT2CO2pe2BSbEoKc")
+# API KEY
+API_KEY = "AIzaSyAXA7edsGE5ggJrQo5wT2CO2pe2BSbEoKc"
 
-for m in genai.list_models():
-    print(m.name)
+genai.configure(api_key=API_KEY)
+
+# MODEL (senin denemek istediğin)
+model = genai.GenerativeModel("models/gemini-1.5-flash-002")
+
+st.title("Gemini Test")
+
+if st.button("Test Et"):
+    try:
+        response = model.generate_content("Merhaba, kısa bir analiz yaz")
+        st.success(response.text)
+
+    except Exception as e:
+        st.error(str(e))
